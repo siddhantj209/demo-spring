6e885ae53c4d46d42139a4ae3e4009d22cb9a1a6
siddhant619 <siddhantj6.023@gmail.com>
2024-12-18 13:59:31 +0530

    Added post endpoint

--- src/main/java/com/sid/demo/controller/TestController.java
+++ src/main/java/com/sid/demo/controller/TestController.java
@@ -1,12 +1,18 @@
 package com.sid.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
     @GetMapping("/test")
     public void test() {
+        System.out.println("in test");
+    }
+
+    @PostMapping("/test")
+    public void testpost() {
         System.out.println("in test");
     }
 }
