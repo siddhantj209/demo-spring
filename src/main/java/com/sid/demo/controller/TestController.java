85d5a4ebe3297d02be1f6b1ec6031c3bf4258bf5
siddhant619 <siddhantj6.023@gmail.com>
2024-12-18 14:37:21 +0530

    Added slf4j

--- src/main/java/com/sid/demo/controller/TestController.java
+++ src/main/java/com/sid/demo/controller/TestController.java
@@ -1,9 +1,11 @@
 package com.sid.demo.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class TestController {
     @GetMapping("/test")
