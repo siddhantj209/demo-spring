af1760d99f8040af263880dfa4b218f4a922eb1d
Devanshu <siddhantj6.023@gmail.com>
2024-12-21 13:58:48 +0530

    AR-2000-added delete endpoint

--- src/main/java/com/sid/demo/controller/TestController.java
+++ src/main/java/com/sid/demo/controller/TestController.java
@@ -1,12 +1,18 @@
 package com.sid.demo.controller;
 
+import com.sid.demo.service.DemoService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 public class TestController {
+
+    @Autowired
+    private DemoService demoService;
+
     @GetMapping("/test")
     public ResponseEntity<String> test() {
         System.out.println("in test ");
@@ -16,5 +22,10 @@ public class TestController {
     @PostMapping("/test")
     public void testpost(@RequestParam int age, @RequestParam String name) {
         System.out.println("Name and Age recieved: " + name + age);
+    }
+
+    @DeleteMapping("/")
+    public void delete(String id) {
+        demoService.delete(id);
     }
 }
