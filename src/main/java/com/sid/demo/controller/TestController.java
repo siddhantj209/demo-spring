dc3432ccbec8e46ec55fa4bd6caf9c9c7989eb17
siddhantj209 <siddhantj6.022@gmail.com>
2024-12-21 13:27:41 +0530

    Accepting age

--- src/main/java/com/sid/demo/controller/TestController.java
+++ src/main/java/com/sid/demo/controller/TestController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -16,7 +17,7 @@ public class TestController {
     }
 
     @PostMapping("/test")
-    public void testpost() {
-        System.out.println("in test post endpoint");
+    public void testpost(@RequestBody int age) {
+        System.out.println("Age recieved: " + age);
     }
 }
